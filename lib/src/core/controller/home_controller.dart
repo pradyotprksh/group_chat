055b46736519b72d20b78046d3f0994e6f1b1a00
pradyotprksh <pradyot@mobifyi.com>
2020-10-09 13:24:54 +0530

    Build uploaded on play store

--- lib/src/core/controller/home_controller.dart
+++ lib/src/core/controller/home_controller.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
-import 'package:group_chat/src/util/firestore_constants.dart';
-import 'package:group_chat/src/util/string.dart';
 
 class HomeController extends GetxController {
   var currentIndex = 0;
@@ -22,35 +18,7 @@ class HomeController extends GetxController {
   }
 
   void setupNotification() async {
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS = IOSInitializationSettings();
-    var initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
-    FirebaseFirestore.instance
-        .collection(FirestoreConstants.USER)
-        .doc(FirebaseAuth.instance.currentUser.uid)
-        .get()
-        .then((value) {
-      if (value.get(FirestoreConstants.GET_DAILY_NOTIFICATION)) {
-        var time = Time(22, 0, 0);
-        var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-            StringConstant.APP_NAME,
-            StringConstant.APP_NAME,
-            StringConstant.APP_NAME);
-        var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-        var platformChannelSpecifics = NotificationDetails(
-            androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-        flutterLocalNotificationsPlugin.showDailyAtTime(
-            0,
-            'Daily Game',
-            'Join others for playing the daily game and rise into the leader board.',
-            time,
-            platformChannelSpecifics);
-      }
-    });
   }
 
   void disableNotification() async {
